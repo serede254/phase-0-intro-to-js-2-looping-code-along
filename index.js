d7e825738de95f397e38d58e480cceb7735ea126
root <root@SURFACE.>
2024-03-20 15:15:02 +0300

    update

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+const people=["Charlie","Samip","Ali"];
+function writeCards(people,surprise){
+    const message=[];
+    for(let i=0;i<people.length;i++){
+        message.push(`Thank you, ${people[i]}, for the wonderful ${surprise} gift!`)
+   
+    }
+return message;
+}
+function countDown(){
+let i =0;
+while(i<11){
+    console.log(i++)
+}
+return countDown;
+}
